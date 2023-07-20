0e57bae8e54ff3620911c031f3add6d01c655396
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-07-20 14:00:11 -0600

    -- Organized project for Day1

---
+++ Day 1/Day1Exercise2.py
@@ -0,0 +1,5 @@
+
+print("Day 1 - String Manipulation")
+print("String Concatenation is done with the "+" sign.")
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backlash and n.")
