33e850f7b29016f1a2ec6fc4aaf88e45343cc63e
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-07-21 08:24:25 -0600

    -- F- Strings for different types

--- Day 2/NumberManipulation.py
+++ Day 2/NumberManipulation.py
@@ -1,2 +1,24 @@
 
-print(round(8 / 3))
+    # type is float
+print(round(8 / 3, 2))
+
+# floor divison
+    # type is int
+print(8 // 3)
+
+result = 4 / 2
+result /= 2
+print(result)
+
+# example with user score
+
+#user scores point
+
+# F string
+
+score = 0
+height = 1.8
+isWinning = True
+
+print(f"your score is {score}, your height is {height}, you are winning is {isWinning}")
+
