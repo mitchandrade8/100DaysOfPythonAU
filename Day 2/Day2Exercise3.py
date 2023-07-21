eb047a39629b4d7926dc7e9ef2ed73202c336290
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-07-21 08:50:12 -0600

    -- Finished - Days remaining in life

--- Day 2/Day2Exercise3.py
+++ Day 2/Day2Exercise3.py
@@ -7,12 +7,14 @@ age = input("What is your current age? ")
 
 timeRemaining = maxAge - int(age)
 
+print(f"{timeRemaining} years left to live ")
+
 timeRemaining_conversion_days = round(timeRemaining*365)
 print(f"{timeRemaining_conversion_days} days remaining in your life ")
 
 timeRemaining_conversion_weeks = round((timeRemaining*365)/4)
 print(f"{timeRemaining_conversion_weeks} weeks remaining in your life ")
 
+timeRemaining_conversion_months = round((timeRemaining*365)/12)
+print(f"{timeRemaining_conversion_months} months remaining in your life ")
 
-timeRemaining_conversion_months = round(timeRemaining*365)
-print(f"{timeRemaining} years left to live ")
