5d521749808e9b4f1c6926624e9db8b462df5bd6
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-07-24 10:53:59 -0600

    updating to --Start day 3

--- Day 2/TipCalculator.py
+++ Day 2/TipCalculator.py
@@ -1,1 +1,9 @@
 
+
+var calculatorTotal = 0.0
+
+print("Welcome to the tip calculator.")
+print(input("What was the total bill? "))
+print(input("What tip would you like to give? 10, 12, or 15? "))
+print(input("How many people to split the bill? "))
+printf("Each person should pay: /()")
