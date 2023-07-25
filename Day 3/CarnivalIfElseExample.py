ede948433a737a31b8393e548360c5f071d3bcc0
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-07-25 14:17:21 -0600

    -- Conditional Operators in Python

---
+++ Day 3/CarnivalIfElseExample.py
@@ -0,0 +1,9 @@
+
+print("Welcome to the rollercoaster!")
+
+height = int(input("What is your hieght in cm? "))
+
+if height < 120:
+    print("Sorry, you have to grow taller before you can ride!")
+else:
+    print("You can ride the rollercoaster!")
