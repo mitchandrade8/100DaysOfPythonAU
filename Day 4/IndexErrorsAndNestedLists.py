8f0eb4fcf1e2a7763595ed61bb6823d4ebae26e7
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-07-27 12:54:23 -0600

    -- Index Errors

--- Day 4/IndexErrorsAndNestedLists.py
+++ Day 4/IndexErrorsAndNestedLists.py
@@ -1,1 +1,16 @@
 
+states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut", "Massachusetts", 
+                     "Maryland", "South Carolina", "New Hampshire", "Virginia", "New York", "North Carolina", 
+                     "Rhode Island", "Vermont", "Kentucky", "Tennessee", "Ohio", "Louisiana", "Indiana", "Mississippi",
+                       "Illinois", "Alabama", "Maine", "Missouri", "Arkansas", "Michigan", "Florida", "Texas", "Iowa", 
+                       "Wisconsin", "California", "Minnesota", "Oregon", "Kansas", "West Virginia", "Neveda", "Nebraska", 
+                       "Colorado", "North Dakota", "South Dakota", "Montana", "Washington", "Idaho", "Wyoming", "Utah", 
+                       "Oklahoma", "New Mexico", "Arizona", "Alaska", "Hawaii"]
+
+num_of_states = len(states_of_america)
+
+print(states_of_america[48])
+
+dirty_dozen = ["Strawberries", "Spinach", "Kale", "Nectrines", "Apples", "Grapes", "Peaches", 
+               "Cherries", "Pears", "Tomatoes", "Celery", "Potatoes"]
+
