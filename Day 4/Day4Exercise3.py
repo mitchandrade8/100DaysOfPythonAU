c006d34cb3c720ccfd7b15126a07cbf531d59d07
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-07-27 13:16:41 -0600

    -- Start of more complex exercise

---
+++ Day 4/Day4Exercise3.py
@@ -0,0 +1,19 @@
+
+row1 = ["⬜️","️⬜️","️⬜️"]
+row2 = ["⬜️","⬜️","️⬜️"]
+row3 = ["⬜️️","⬜️️","⬜️️"]
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("Where do you want to put the treasure? ")
+
+horizontal = int(position[0])
+vertical = int(position[1])
+
+print(map[3 - 1])
+
+
+
+
+
+
+print(f"{row1}\n{row2}\n{row3}")
