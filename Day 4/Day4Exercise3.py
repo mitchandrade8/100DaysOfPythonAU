fa1947cc5d13a7850de5d339747e94069d300dbd
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-07-27 15:05:44 -0600

    -- Refining my problem solving skills

--- Day 4/Day4Exercise3.py
+++ Day 4/Day4Exercise3.py
@@ -2,18 +2,20 @@
 row1 = ["⬜️","️⬜️","️⬜️"]
 row2 = ["⬜️","⬜️","️⬜️"]
 row3 = ["⬜️️","⬜️️","⬜️️"]
+
+
 map = [row1, row2, row3]
 print(f"{row1}\n{row2}\n{row3}")
+
+
 position = input("Where do you want to put the treasure? ")
 
+#converting input to location
 horizontal = int(position[0])
 vertical = int(position[1])
 
-print(map[3 - 1])
-
-
-
-
-
+# appending 'X' to that location
+selected_row = map[vertical - 1]
+selected_row[horizontal - 1] = 'X'
 
 print(f"{row1}\n{row2}\n{row3}")
