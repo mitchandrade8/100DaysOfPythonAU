c91175ba0886ff4ffba99fdf5b7a723e010d7c66
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-07-27 23:00:58 -0600

    -- Finished FizzBuzz Challenge

--- Day 5/FixBuzz.py
+++ Day 5/FixBuzz.py
@@ -3,13 +3,11 @@
 
 
 for number in range(1, 101):
-    if number % 3 == 0:
-        print("Fizz")
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
     elif number % 5 == 0:
         print("Buzz")
-    elif number % 3 and number % 5:
-        print("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
     else:
         print(number)
-
-print(number)
