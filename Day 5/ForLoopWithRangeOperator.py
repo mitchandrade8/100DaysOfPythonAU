12efc5be2438acdb0550c4306165b14bf9cf9b64
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-07-27 18:05:37 -0600

    -- Finish Range in For loop

--- Day 5/ForLoopWithRangeOperator.py
+++ Day 5/ForLoopWithRangeOperator.py
@@ -4,3 +4,12 @@
 #for loop with Range EX
 for number in range(1, 10):
     print(number)
+
+
+
+# example with 1 - 100
+
+total = 0 
+for number in range(1, 101):
+    total += number
+print(total)
--- Day 5/HighScoreExercise.py
+++ Day 5/HighScoreExercise.py
@@ -8,10 +8,11 @@ print(student_scores)
 print(max(student_scores))
 
 # Under the hood method
-
+    # created a value to store the highest value while we loop through our array of scores
 highest_score = 0
 
 for score in student_scores:
+    # we check the new value while looping through against the last score to update the highest score
     if score > highest_score:
         highest_score = score
 print(f"The highest score in the class is: {highest_score}")
