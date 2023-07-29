cc11254a2329178957d078cd81579dddd3e71c29
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-07-29 16:25:33 -0600

    -- Start of hangman challenge and breaking it down

---
+++ Day 7/Day7Challenge1.py
@@ -0,0 +1,22 @@
+
+import random
+# Step 1
+
+word_list = ["ardvark", "baboon", "camel"]
+
+# Todo - 1: Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+
+# Todo - 2: Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+
+# Todo - 3: Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+
+
+## Looping Through the array and assign to the variable **chosen_word
+
+for word in word_list:
+    chosen_word = random.randint[word_list]
+    print(chosen_word)
+
+
+
+guess = ""
