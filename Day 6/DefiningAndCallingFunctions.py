287edaa0402e5ea9d823f47de1072b9bf0d55c4f
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-07-29 14:31:51 -0600

    -- Start of day 6

--- Day 6/DefiningAndCallingFunctions.py
+++ Day 6/DefiningAndCallingFunctions.py
@@ -5,3 +5,12 @@ def my_function():
     print("Bye")
 
 my_function()
+
+# two steps to a function
+
+    # Step 1: Define function
+
+    # Step 2: Calling the function
+
+#functions can make code more readable
+
