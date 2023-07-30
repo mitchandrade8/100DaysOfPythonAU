4345a357b3957fbd5c9983364b493367ce979019
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-07-30 00:41:05 -0600

    -- Functions with inputs

--- Day 8/FunctionsWithInputs.py
+++ Day 8/FunctionsWithInputs.py
@@ -6,3 +6,12 @@ def greet():
 
 greet()
 greet()
+
+## Function that allows for input
+
+def greet_with_name(name):
+    print(f"Hello {name}")
+    print(f"How do you do {name}?") 
+
+greet_with_name("Mitch")
+
