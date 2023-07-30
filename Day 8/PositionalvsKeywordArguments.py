bdba23b2a77e1f4656adc2ccc56f2b1e5e6faed5
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-07-30 00:58:59 -0600

    -- Functions with several parameters

---
+++ Day 8/PositionalvsKeywordArguments.py
@@ -0,0 +1,8 @@
+
+# Functions with more than one input
+
+def greet_with(name, location):
+    print(f"Hello {name}! ")
+    print(f"What is the weather like at {location}? ")
+
+greet_with("Mitch", "Denver, Co")
