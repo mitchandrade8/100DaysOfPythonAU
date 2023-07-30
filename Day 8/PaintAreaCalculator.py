bc1682bc7c4099bb6be02e03d55b20384acf4a7b
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-07-30 03:34:47 -0600

    -- Finish of Cans of paint

--- Day 8/PaintAreaCalculator.py
+++ Day 8/PaintAreaCalculator.py
@@ -1,4 +1,6 @@
 
+import math
+
 # You are painting a wall. The instructions on the paint can says that '1 can of paint can cover 5 square meters' of wall. 
 # given a random height and width of wall, calculate how many cans of paint you'll need to buy
 
@@ -11,9 +13,19 @@
 
 print("Hello, please help us calculate how many cans of paint you need by providing height and width!")
 
+def paint_calculation(height, width, cover):
+    area = height * width
+    number_of_cans = math.ceil(area / cover)
+    print(f"You will need {number_of_cans} cans of paint.")
+
+
+
 test_h = int(input("Height of wall: "))
 test_w = int(input("Width of wall: "))
 coverage = 5
 
 paint_calculation(height=test_h, width=test_w, cover = coverage)
+
+
+
 
