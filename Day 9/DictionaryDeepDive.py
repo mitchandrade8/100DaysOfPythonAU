12739d8238db68d6c92cc3d1d24e600faa2a5f1b
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-07-31 15:02:24 -0600

    -- Retreiving an value from a dictionary

--- Day 9/DictionaryDeepDive.py
+++ Day 9/DictionaryDeepDive.py
@@ -1,1 +1,20 @@
 
+# dictionaries in python:
+    # Group and tag information
+    # **** Dictionaries are Key/Value pairs ****
+
+# Examples:
+
+# Key : Value 
+
+# Bug : "An error in a program that prevents the program from running as expected."
+
+programming_dictionary = {
+    "Bug": "An error in a program that prevents the program from running as expected.",
+    "Function": "A piece of code that you can easily call over and over again.",
+    "Loop": "The action of doing something over and over again."
+}
+
+# Want to retrieve an item from the dictionary:
+
+print(programming_dictionary["Bug"])
