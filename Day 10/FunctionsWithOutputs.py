1fbf39567b679b7b01fa28e9b4ec38244c3c90ff
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-08-01 03:38:59 -0600

    -- Formatting Strings

--- Day 10/FunctionsWithOutputs.py
+++ Day 10/FunctionsWithOutputs.py
@@ -13,3 +13,25 @@
 
 # Final level of functions 
 
+# Example of function with Output
+
+def my_function():
+    result = 3 * 2
+    return print(result)
+
+my_function()
+
+## Deep Dive Into Functions with Outputs
+
+def format_name(first_Name, last_Name):
+
+    # Convert strings 
+    formated_f_name = first_Name.title()
+    formated_l_name = last_Name.title()
+
+    print(f"{formated_f_name} {formated_l_name}")
+
+format_name("mitch", "andrade")
+
+
+
