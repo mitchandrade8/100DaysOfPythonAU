a00321b42ba7d724a25f757ca00bbe4c8bf50f97
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-08-01 13:12:05 -0600

    -- Start: days in month challenge

--- Day 10/MultipleReturnValues.py
+++ Day 10/MultipleReturnValues.py
@@ -4,7 +4,7 @@
 def format_name(f_name, l_name):
     # Handles if user does not enter anything
     if f_name == "" or l_name == "":
-        return
+        return "You did not provide valid inputs!"
     formated_f_name = f_name.title()
     formated_l_name = l_name.title()
     return f"Result: {formated_f_name} {formated_l_name}"
