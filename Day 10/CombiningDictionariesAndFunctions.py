469cf71f13934c37c6ba22711fa7cfd5fb950b05
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-08-01 16:16:54 -0600

    -- Finish Calculator example

--- Day 10/CombiningDictionariesAndFunctions.py
+++ Day 10/CombiningDictionariesAndFunctions.py
@@ -27,10 +27,15 @@ operations = {
 
 num1 = int(input("What is the first number?: "))
 
-num2 = int(input("What is the second number? "))
-
 for symbol in operations:
     print(symbol)
 
 operation_symbol = input("Pick an operation from the line above: ")
 
+num2 = int(input("What is the second number? "))
+
+
+calculation_function = operations[operation_symbol]
+answer = calculation_function(num1, num2)
+
+print(f"{num1} {operation_symbol} {num2} = {answer}")
