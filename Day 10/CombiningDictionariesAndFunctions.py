04b0383438717d88b4357fcd6f73559c2bc95e43
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-08-01 13:47:01 -0600

    -- Start: Calculator Challenge

---
+++ Day 10/CombiningDictionariesAndFunctions.py
@@ -0,0 +1,19 @@
+
+# Core Functions
+
+# Addition
+def add(n1, n2):
+    return n1 + n2
+
+# Subtraction
+def subtract(n1, n2):
+    return n1 - n2
+
+# Multiplication
+def multiply(n1, n2):
+    return n1 * n2
+
+# Division
+def divide(n1, n2):
+    return n1 / n2
+
