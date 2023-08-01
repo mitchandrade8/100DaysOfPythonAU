61deb417210c91408d7fd2c5a765640911ffd437
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-08-01 03:26:38 -0600

    --Updating to functions with Outputs

--- Day 10/FunctionsWithOutputs.py
+++ Day 10/FunctionsWithOutputs.py
@@ -5,3 +5,11 @@
         # Then do this
         # Finally do this
 
+# Overview of funcions with Inputs
+    # def my_function(something):
+        # Do this with something
+        # Then do this 
+        # Finally do this
+
+# Final level of functions 
+
--- Day 9/NestingListsAndDictionaries.py
+++ Day 9/NestingListsAndDictionaries.py
@@ -29,8 +29,16 @@ travel_log = {
 # Nesting Dictionary in a list
 
 travel_log = [
-     {"country": "France", "cities_visited": ["Paris", "Lille", "Dijon"], "total_visits": 12},
-     {"country": "Germany", "cities_visited": ["Berlin", "Hamburg", "Stuttgart"], "total_visits": 5},
+    {
+        "country": "France", 
+        "cities_visited": ["Paris", "Lille", "Dijon"], 
+        "total_visits": 12
+    },
+    {
+        "country": "Germany", 
+        "cities_visited": ["Berlin", "Hamburg", "Stuttgart"], 
+        "total_visits": 5
+    },
 ]
 
    
