10b1506304bf6b461083fb1c443d3bbea6bfbee0
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-08-01 13:03:45 -0600

    -- Empty State for input

--- Day 10/MultipleReturnValues.py
+++ Day 10/MultipleReturnValues.py
@@ -2,9 +2,12 @@
 # Analysis of return and scope
 
 def format_name(f_name, l_name):
+    # Handles if user does not enter anything
+    if f_name == "" or l_name == "":
+        return
     formated_f_name = f_name.title()
     formated_l_name = l_name.title()
-    return f"{formated_f_name} {formated_l_name}"
+    return f"Result: {formated_f_name} {formated_l_name}"
 
     # Print will not be called
         # print(formated_f_name)
