03ea976046ede809bfe1e0f3d77c19d389b48058
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-08-01 16:05:45 -0600

    -- Deep dive in calculator

--- Day 10/CombiningDictionariesAndFunctions.py
+++ Day 10/CombiningDictionariesAndFunctions.py
@@ -17,3 +17,20 @@ def multiply(n1, n2):
 def divide(n1, n2):
     return n1 / n2
 
+# Dictionary
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
+}
+
+num1 = int(input("What is the first number?: "))
+
+num2 = int(input("What is the second number? "))
+
+for symbol in operations:
+    print(symbol)
+
+operation_symbol = input("Pick an operation from the line above: ")
+
