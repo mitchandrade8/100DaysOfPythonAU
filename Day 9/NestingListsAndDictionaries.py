7d8eefe80378e1e4e306033107d0accba75ce766
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-08-01 00:19:14 -0600

    --Start of nesting/dictionaries

--- Day 9/NestingListsAndDictionaries.py
+++ Day 9/NestingListsAndDictionaries.py
@@ -1,1 +1,27 @@
 
+# Example
+# {
+# Key: [List],
+# Key2: {Dict},
+# }
+
+# Nesting 
+
+capitals = {
+    "France": "Paris",
+    "Germany": "Berlin",
+}
+
+# Nesting a list in a dictionary
+
+travel_log = {
+    "France": ["Paris", "Lille", "Dijon"],
+    "Germany": ["Berlin", "Hamburg", "Stuttgart"],
+}
+
+# Nesting Dictionary in a Dictionary
+
+travel_log = {
+    "France": {"cities_visited": ["Paris", "Lille", "Dijon"], "total_visits": 12},
+    
+}
