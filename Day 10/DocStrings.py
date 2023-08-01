10f777cf6c2e354624b0a4662504311f2e4c0bad
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-08-01 13:41:23 -0600

    -- Finished DocStrings

--- Day 10/DocStrings.py
+++ Day 10/DocStrings.py
@@ -2,3 +2,8 @@
 # Docstrings : ways to create little bits of documentation as we're
 # coding along in our functions or in our other blocks of code.
 
+"""Take a first and last name and format it to return the title
+case version of the name.
+"""
+
+# example of docstring 
