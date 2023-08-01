ae7c1b1d576b20a6117192ef844f8faa1552f38b
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-08-01 13:31:01 -0600

    -- Finished Leap year

--- Day 10/DaysInMonth.py
+++ Day 10/DaysInMonth.py
@@ -3,16 +3,26 @@ def is_leap(year):
     if year % 4 == 0:
         if year % 100 == 0:
             if year % 400 == 0:
-                print("Leap year.")
+                return True
+                # print("Leap year.")
             else:
-                print("Not leap year.")
+                return False
+                # print("Not leap year.")
         else: 
-            print("Leap year.")
+            return True
+            # print("Leap year.")
     else:
-        print("Not leap year.")
+        return False
+        # print("Not leap year.")
 
-def days_in_month():
+def days_in_month(year, month):
+    if month > 12 or month < 1:
+        return "Invalid month"
     month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    if is_leap(year) and month == 2:
+        return 29
+    return month_days[month - 1]
+
 
 # Given code
 year = int(input("Enter a year: "))
