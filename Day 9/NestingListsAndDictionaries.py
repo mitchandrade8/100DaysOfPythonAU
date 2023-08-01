6e175d3dfb036596f2cb8376e3739a5c97311a55
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-08-01 01:02:05 -0600

    -- Dictionary in a list

--- Day 9/NestingListsAndDictionaries.py
+++ Day 9/NestingListsAndDictionaries.py
@@ -23,5 +23,14 @@ travel_log = {
 
 travel_log = {
     "France": {"cities_visited": ["Paris", "Lille", "Dijon"], "total_visits": 12},
-    
+    "Germany": {"cities_visited": ["Berlin", "Hamburg", "Stuttgart"], "total_visits": 5},
 }
+
+# Nesting Dictionary in a list
+
+travel_log = [
+     {"country": "France", "cities_visited": ["Paris", "Lille", "Dijon"], "total_visits": 12},
+     {"country": "Germany", "cities_visited": ["Berlin", "Hamburg", "Stuttgart"], "total_visits": 5},
+]
+
+   
