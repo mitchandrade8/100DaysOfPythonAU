bc727f9fe8c270796c8fea2d2a2e6f78ab4452cd
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-08-03 16:10:13 -0600

    -- Examples of scope and adjustments for them

--- Day 12/BlockScope.py
+++ Day 12/BlockScope.py
@@ -5,9 +5,20 @@
 
 game_level = 3
 
-enemies = ["Skeleton", "Zombie", "Alien"]
-if game_level < 5:
-    new_enemy = enemies[0]
+## Before: 
 
+#enemies = ["Skeleton", "Zombie", "Alien"]
+#if game_level < 5:
+    #new_enemy = enemies[0]
 
-print(new_enemy)
+#print(new_enemy)
+
+## After: 
+
+def create_enemy():
+    enemies = ["Skeleton", "Zombie", "Alien"]
+    if game_level < 5:
+        new_enemy = enemies[0]
+
+    print(new_enemy)
+
