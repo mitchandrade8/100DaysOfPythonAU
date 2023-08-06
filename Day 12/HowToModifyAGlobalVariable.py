7794a94f9de6ea09ef3e4fc552b9334b3d73d1d4
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-08-06 01:59:47 -0600

    -- Global Scope in python, modify it with return

--- Day 12/HowToModifyAGlobalVariable.py
+++ Day 12/HowToModifyAGlobalVariable.py
@@ -3,8 +3,11 @@
 enemies = 1
 
 def increase_enemies():
-    enemies = 2
+    # enemies = 2
     print(f"enemies inside function: {enemies}")
+    return enemies + 1
 
-increase_enemies()
+enemies = increase_enemies()
 print(f"enemies outside function: {enemies}")
+
+# Community will tell you to use global to read or use // but do not modify a global variable in Python
