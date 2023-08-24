b62de8a0bc02a2b105e29b13dca22be5e9c2fc12
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-08-24 11:54:43 -0600

    -- Starting breakdown of guess number challenge

--- Day 12/NumberGuessingGame.py
+++ Day 12/NumberGuessingGame.py
@@ -1,1 +1,12 @@
 
+from random import randint
+# Guess the number game
+
+# need a vaariable to track the difficulty
+# varaible to for the guess
+# if the guess is correct of not 
+    # if else 
+
+# Choosing a random number between 1 and 100.
+
+answer = randint(1)
