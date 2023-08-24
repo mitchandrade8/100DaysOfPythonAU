1bea34f752805aacf4da29452f59fbf776e3ebf5
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-08-24 14:44:42 -0600

    -- Function and logic for guessing

--- Day 12/NumberGuessingGame.py
+++ Day 12/NumberGuessingGame.py
@@ -7,6 +7,27 @@ from random import randint
 # if the guess is correct of not 
     # if else 
 
-# Choosing a random number between 1 and 100.
 
-answer = randint(1)
+# Function to check user's guess against actual answer
+def check_answer(guess, answer):
+    if guess > answer:
+        print("Too high.")
+    elif guess < answer:
+        print("Too low.")
+    else: 
+        print(f"You got it! The answer was {answer}.")
+# Choosing a random number between 1 and 100
+
+print("Welcome to the Number Guessing Game!")
+print("I'm thinking of a number between 1 and 100.")
+answer = randint(1, 100)
+
+# Function for difficulty
+
+# Let the user guess a number
+guess = int(input("Make a guess:"))
+
+
+# Track the number of turns and reduce by 1 if they get it wrong
+
+# Repeat the guessing functionality if they get it wrong
