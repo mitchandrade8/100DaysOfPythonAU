9f7cf715f3fa0957ad5b005f661976502870bbe6
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-08-25 08:15:06 -0600

    -- Updating number challenge

--- Day 12/NumberGuessingGame.py
+++ Day 12/NumberGuessingGame.py
@@ -2,11 +2,8 @@
 from random import randint
 # Guess the number game
 
-# need a vaariable to track the difficulty
-# varaible to for the guess
-# if the guess is correct of not 
-    # if else 
-
+EASY_LEVEL_TURNS = 10
+HARD_LEVEL_TURNS = 5
 
 # Function to check user's guess against actual answer
 def check_answer(guess, answer):
@@ -23,11 +20,17 @@ print("I'm thinking of a number between 1 and 100.")
 answer = randint(1, 100)
 
 # Function for difficulty
+def set_difficulty():
+    level = input("Choose a difficulty. Type 'easy' or 'hard': ")
+    if level == "easy":
+        return EASY_LEVEL_TURNS
+    else:
+        return HARD_LEVEL_TURNS
 
 # Let the user guess a number
-guess = int(input("Make a guess:"))
-
-
+guess = int(input("Make a guess: "))
+turns = set_difficulty
+print(f"You have {turns} attempts remaining to guess the number.")
 # Track the number of turns and reduce by 1 if they get it wrong
 
 # Repeat the guessing functionality if they get it wrong
