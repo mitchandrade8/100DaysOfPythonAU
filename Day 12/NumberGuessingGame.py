80fb5489ff12b73c5ca0eba7c90011080508b151
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-08-25 18:09:45 -0600

    --More logic and functionality added

--- Day 12/NumberGuessingGame.py
+++ Day 12/NumberGuessingGame.py
@@ -5,12 +5,15 @@ from random import randint
 EASY_LEVEL_TURNS = 10
 HARD_LEVEL_TURNS = 5
 
+
 # Function to check user's guess against actual answer
-def check_answer(guess, answer):
+def check_answer(guess, answer, turns):
     if guess > answer:
         print("Too high.")
+        return turns -1
     elif guess < answer:
         print("Too low.")
+        return turns - 1
     else: 
         print(f"You got it! The answer was {answer}.")
 # Choosing a random number between 1 and 100
@@ -27,16 +30,17 @@ def game():
     print("Welcome to the Number Guessing Game!")
     print("I'm thinking of a number between 1 and 100.")
     answer = randint(1, 100)
-    turns = set_difficulty()
 
-    print(f"You have {turns} attempts remaining to guess the number.")
+    turns = set_difficulty()
+    
 
     guess = 0
     while guess != answer:
     # Let the user guess a number
+        print(f"You have {turns} attempts remaining to guess the number.")
         guess = int(input("Make a guess: "))
 
-        check_answer(guess, answer)
+        turns = check_answer(guess, answer, turns)
 
 # Track the number of turns and reduce by 1 if they get it wrong
 
